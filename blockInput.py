d33ce38fe09034710612161798c5a081cd581e46
Jake <jsaunders799@lsr7.net>
2017-11-30 16:47:49 +0000

    11/30

---
+++ blockInput.py
@@ -0,0 +1,13 @@
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+blockType = int(input('Enter a block type: '))
+pos = mc.player.getTilePos()
+x = pos.x
+y = pos.y
+z = pos.z
+try:
+    blockType = int(input('Enter a block type: ')
+    mc.setBlock(x, y, z, blockType)
+except:
+    mc.postToChat('You did not enter a number! Enter a number next time.')
+                    
